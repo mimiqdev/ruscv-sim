fd8a67843aa813ed9ec69cf92208e4e88415c900
Mimikyu Dev <mail@mimikyu.dev>
2026-01-31 09:06:15 +0000

    style: fix cargo fmt ordering

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@ pub mod execute;
 pub mod memory;
 pub mod tlm;
 
-pub use ruscv_macros::*;
 pub use core::{CoreState, PrivilegeMode, RiscvCore};
 pub use csr::{CsrError, CsrFile};
 pub use decode::{DecodeError, DecodedInstruction, InstructionDecoder, InstructionFormat};
 pub use execute::{ExecuteError, Executor};
 pub use memory::{MemoryError, MemoryInterface, SimpleMemory};
+pub use ruscv_macros::*;
 pub use tlm::{TlmCommand, TlmGenericPayload, TlmInterface, TlmPhase, TlmResponseStatus, TlmTime};
